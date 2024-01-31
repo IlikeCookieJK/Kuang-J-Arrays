93c318ef5c597deb6bf2a4a9b936da300b03da2d
Jun <jun_hong.kuang23@student.cart.org>
2024-01-31 13:08:20 -0800

    js

---
+++ Kuang-J-Arrays.js
@@ -0,0 +1,33 @@
+// array is used to store organized information
+// New year's resolution:
+// Keep a journal
+// Take a falconry
+// Learn Kubernetes
+// computer count from zero '0'
+// an array can store any type of information
+// index is the postion of the items in an array
+// array literal is create by using square brackets []
+// every thing in an array is seperated by a comma
+// use const instead of let or var
+// use a number inside of bracket to call specific item in an array
+// if there is no item in the array it will return undefine
+
+
+
+let newYearsResolutions = ['Keep a journal', 'Take a falconry class', 'Learn Kubernetes'];
+
+const hobbies = [`Azur Lane`, `League of Legends`, `Valorant`, `Anime`];
+// console.log(hobbies[0]);
+
+let listitem = hobbies[0];
+console.log(listitem);
+console.log(hobbies[4]);
+
+let seasons = [`Winter`, `Spring`, `Summer`, `Fall`];
+seasons[3] = `Autumn`;
+console.log(seasons);
+
+newYearsResolutions[0] = `Sleep`;
+newYearsResolutions[1] = `Game`;
+newYearsResolutions[2] = `Watch Anime`;
+console.log(newYearsResolutions);
